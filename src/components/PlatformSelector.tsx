4f457f0a1c12d34ad385c94881c40813774a3558
Yuragranovsky <yuragranovsky@gmail.com>
2025-05-23 15:19:37 +0300

    PlatformSelector - ChevronUp/ChevronDown, item Platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,38 +1,69 @@
-import { Menu, Button, Portal, Spinner } from '@chakra-ui/react'
-import {FaChevronDown} from 'react-icons/fa'
-import usePlatforms from '../hooks/usePlatforms';
-import { FC } from 'react';
-import ParentPlatform from '../model/ParentPlatform';
+import { Menu, Button, Portal, Spinner } from "@chakra-ui/react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import usePlatforms from "../hooks/usePlatforms";
+import { FC, useState } from "react";
+import ParentPlatform from "../model/ParentPlatform";
 interface Props {
-    selectedPlatform: ParentPlatform | null;
-    onSelectPlatform: (platform: ParentPlatform) => void
+  selectedPlatform: ParentPlatform | null;
+  onSelectPlatform: (platform: ParentPlatform | null) => void;
 }
-const PlatformSelector: FC<Props> = ({selectedPlatform, onSelectPlatform}) => {
-    const {errorMessage, isLoading, data:platforms} = usePlatforms();
+const PlatformSelector: FC<Props> = ({
+  selectedPlatform,
+  onSelectPlatform,
+}) => {
+  const { errorMessage, isLoading, data: platforms } = usePlatforms();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <>
-    { isLoading?  <Spinner></Spinner> :
-    !errorMessage && 
-        <Menu.Root>
-      <Menu.Trigger asChild>
-        <Button variant="outline" size="sm" borderWidth={0}> 
-         {selectedPlatform?.name || "Platforms"}
-          <FaChevronDown/>
-        </Button>
-      </Menu.Trigger>
-      <Portal>
-        <Menu.Positioner>
-          <Menu.Content>
-           { platforms.map(p => <Menu.Item key={p.id}onClick={() => onSelectPlatform(p)} value={p.id}>{p.name}</Menu.Item>)}
-            
-          </Menu.Content>
-        </Menu.Positioner>
-      </Portal>
-    </Menu.Root>
-    }
+      {isLoading ? (
+        <Spinner></Spinner>
+      ) : (
+        !errorMessage && (
+          <Menu.Root>
+            <Menu.Trigger asChild>
+              <Button
+                variant="outline"
+                size="sm"
+                borderWidth={0}
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                {selectedPlatform?.name || "Platforms"}
+                {isOpen ? <FaChevronUp /> : <FaChevronDown />}
+              </Button>
+            </Menu.Trigger>
+            <Portal>
+              <Menu.Positioner>
+                <Menu.Content>
+                  <Menu.Item
+                    key={"p.id"}
+                    onClick={() => {
+                      onSelectPlatform(null);
+                      setIsOpen(false);
+                    }}
+                    value={""}
+                  >
+                    Platforms
+                  </Menu.Item>
+                  {platforms.map((p) => (
+                    <Menu.Item
+                      key={p.id}
+                      onClick={() => {
+                        onSelectPlatform(p);
+                        setIsOpen(false);
+                      }}
+                      value={p.id}
+                    >
+                      {p.name}
+                    </Menu.Item>
+                  ))}
+                </Menu.Content>
+              </Menu.Positioner>
+            </Portal>
+          </Menu.Root>
+        )
+      )}
     </>
-    
-  )
-}
+  );
+};
 
-export default PlatformSelector
+export default PlatformSelector;
