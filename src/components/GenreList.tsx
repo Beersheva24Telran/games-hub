83f59586bcccf9a5b82467ac31f059432de82bae
Yuragranovsky <yuragranovsky@gmail.com>
2025-05-23 15:20:14 +0300

    GenreList - item "All Ganres"

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,29 +1,49 @@
-import {  FC } from "react";
+import { FC } from "react";
 import { Text, List, HStack, Avatar, Button, Spinner } from "@chakra-ui/react";
 import useGenre from "../hooks/useGener";
 interface Props {
-  onSelectGenre: (genre: string) => void;
+  onSelectGenre: (genre: string | null) => void;
   selectedGenre: string | null;
 }
-const GenreList: FC<Props> = ({onSelectGenre, selectedGenre}) => {
- const {data: genres, errorMessage, isLoading} = useGenre();
- return isLoading? <Spinner></Spinner> : 
-  (
+const GenreList: FC<Props> = ({ onSelectGenre, selectedGenre }) => {
+  const { data: genres, errorMessage, isLoading } = useGenre();
+  return isLoading ? (
+    <Spinner></Spinner>
+  ) : (
     <>
       {errorMessage ? (
         <Text color="red" fontSize={"2.5rem"}>
           {errorMessage}
         </Text>
       ) : (
-        <List.Root listStyle="none" maxHeight="85vh" overflow="auto" >
+        <List.Root listStyle="none" maxHeight="85vh" overflow="auto">
+          <List.Item key={"g.id"}>
+            <HStack marginStart={"4vw"}>
+              <Button
+                fontWeight={!selectedGenre ? "bold" : "normal"}
+                variant={"outline"}
+                borderWidth="0"
+                onClick={() => onSelectGenre(null)}
+              >
+                All Genres
+              </Button>
+            </HStack>
+          </List.Item>
           {genres.map((g) => (
             <List.Item key={g.id}>
               <HStack padding={2}>
                 <Avatar.Root shape="rounded" size="lg" me="-1">
                   <Avatar.Fallback name={g.name} />
-                  <Avatar.Image src={g.image_background}/>
+                  <Avatar.Image src={g.image_background} />
                 </Avatar.Root>
-                <Button fontWeight={selectedGenre === g.slug ? "bold" : "normal"} variant={"outline"} borderWidth="0" onClick={() => onSelectGenre(g.slug)}>{g.name}</Button>
+                <Button
+                  fontWeight={selectedGenre === g.slug ? "bold" : "normal"}
+                  variant={"outline"}
+                  borderWidth="0"
+                  onClick={() => onSelectGenre(g.slug)}
+                >
+                  {g.name}
+                </Button>
               </HStack>
             </List.Item>
           ))}
